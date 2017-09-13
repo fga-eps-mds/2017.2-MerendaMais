933c0c5b5190e2b65b110f606c7bf4ec304ff51d
Roger Lenke <rogerlenke@gmail.com>
2017-09-13 00:19:17 -0300

    Creating basic structure.

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text, View} from 'react-native' 
+
+class RegisterScreen extends React.Component{
+  render(){
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Olá Mundo!</Text>
+      </View>
+    )
+  }
+}
+
+export default RegisterScreen;
