d5a9149ed833ffad92c88c831be69a2603d5a645
Kamilla Costa Souza <kamillacostasouza@hotmail.com>
2017-09-15 23:51:58 -0300

    #1 - Adding register fields.

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -11,13 +11,44 @@ const styles = {
   fontWeight: 'bold',
   textAlign: "center",
   justifyContent: "center"
-}
+},
+  input:{
+    height:40,
+    backgroundColor: '#fff',
+    marginBotton: 20,
+    color: '#000000',
+  }
 };
 
 export default class RegisterScreen extends React.Component {
   render(){
   return (
+    <View>
     <Text style={styles.Logo}>Merenda+</Text>
+    <TextInput
+    placeholder = "Digite o seu CPF"
+    style={styles.input}
+    />
+    <TextInput
+    placeholder = "Digite o seu nome completo"
+    style={styles.input}
+    />
+    <TextInput
+    placeholder = "Digite o seu email"
+    style={styles.input}
+    />
+    <TextInput
+    placeholder = "Digite o seu telefone"
+    style={styles.input}
+    />
+    <TextInput
+    placeholder = "Escolha seu segmento"
+    style={styles.input}
+    />
+    <TextInput
+    placeholder = "Digite sua senha"
+    style={styles.input}
+    />
+    </View>
   );
-}
 }
