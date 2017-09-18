ec661c2ea1eb231012da7365c90fbdd0dab95515
mlfaa <mlfaa@hotmail.com>
2017-09-17 22:21:53 -0300

    #2 - Adding Header.

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import RegisterScreen from './src/screens/RegisterScreen'
 import LoginConselheiro from './src/screens/LoginConselheiro'
 import TelaInicial from './src/screens/TelaInicial'
+import LoginPresidente from './src/screens/LoginPresidente'
 
 export default class App extends React.Component {
   render() {
     return (
-      <TelaInicial />
+      <LoginPresidente />
     );
   }
 }
