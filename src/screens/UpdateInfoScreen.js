f2ebfcfada61970c32d09a153246a7a5fd3b838a
LucasLermen <lucas.arthur.lermen@gmail.com>
2017-09-20 01:00:23 -0300

    #3 - Adding header, fixing button and layout

--- src/screens/UpdateInfoScreen.js
+++ src/screens/UpdateInfoScreen.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {AppRegistry, Button,Text, TextInput, View } from 'react-native'
+import {StyleSheet, TouchableOpacity, Button, Text, TextInput, View} from 'react-native'
 
 
-class UpdateInfoScreen extends React.Component{
+export default class UpdateInfoScreen extends React.Component{
   constructor(props){
     super(props);
     this.state = {
@@ -13,28 +13,96 @@ class UpdateInfoScreen extends React.Component{
   render(){
     return(
 
-        <View style={{padding: 60,  flex: 3, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'}}>
-          <TextInput
-            style={{height: 60, width: 350, borderWidth: 2, borderRadius: 10}}
-            placeholder="nome@exemplo.com"
-            placeholderTextColor="black"
-            onChangeText={(email) => this.setState({email})}>
-          </TextInput>
-          <TextInput
-            style={{height: 60, width:350, borderWidth: 2, borderRadius: 10}}
-            placeholder="(00)00000-0000"
-            placeholderTextColor="black"
-            onChangeText={(phone) => this.setState({phone})}>
-          </TextInput>
-          <Button
-            style = {{height: 90, width: 390, position: 'absolute', bottom:0, left:0}}
-            title="Concluir"
-            color="#FF9500"
-            />
-      </View>
+        <View style={styles.principal}>
+
+          <View style={styles.topo}>
+            <Text style={styles.textLogo}>EDITAR INFORMAÇÕES</Text>
+          </View>
+
+          <View style={styles.conteudo}>
+            <View style={styles.inputs}>
+              <TextInput
+                placeholder="   nome@exemplo.com"
+                placeholderTextColor="black"
+                onChangeText={(email) => this.setState({email})}>
+              </TextInput>
+            </View>
+            <View style={styles.inputs}>
+              <TextInput
+                placeholder="   (00)00000-0000"
+                placeholderTextColor="black"
+                onChangeText={(phone) => this.setState({phone})}>
+              </TextInput>
+              </View>
+            </View>
+            <TouchableOpacity
+              style= {styles.buttonContainer}>
+              <Text style={styles.buttonText}>Concluir</Text>
+            </TouchableOpacity>
+        </View>
 
     )
   }
 }
 
-export default UpdateInfoScreen;
+const styles = StyleSheet.create({
+
+  topo: {
+      flex: 1.2,
+      backgroundColor: '#FF9500',
+      borderBottomColor: 'black',
+      borderBottomWidth: 1,
+      shadowColor: 'black',
+      shadowOpacity: 1,
+      shadowRadius: 6,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginBottom:50
+    },
+
+    textLogo: {
+        fontSize: 30,
+        color:'white',
+        fontWeight:'bold',
+        marginTop:30,
+    },
+
+    principal: {
+      flex: 1,
+    },
+
+    buttonContainer: {
+      paddingVertical:10,
+      borderWidth: 1,
+      borderRadius: 7,
+      marginHorizontal: 15,
+      marginTop: 30,
+      marginBottom: 20,
+      backgroundColor: '#FF9500',
+      justifyContent: 'flex-end'
+    },
+
+    buttonText:{
+      textAlign: 'center',
+      color: '#FFF'
+    },
+
+    conteudo: {
+
+        marginBottom: 9,
+        flex: 6,
+        flexDirection: 'column'
+
+    },
+
+    inputs: {
+      backgroundColor : '#FAFAFA',
+      paddingVertical:10,
+      borderWidth: 1,
+      borderRadius: 7,
+      justifyContent: 'flex-start',
+      borderColor: 'gray',
+      marginHorizontal: 15,
+      marginBottom: 30
+    }
+});
