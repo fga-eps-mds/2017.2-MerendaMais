00cf67dca629959c189aff611364eecf580e0a38
LucasLermen <lucas.arthur.lermen@gmail.com>
2017-10-06 22:05:14 -0300

    #35 - Fixing spaces, constants and identation errors in UpdateInfoScreen
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>
    Signed-off-by: LucasLermen <lucas.arthur.lermen@gmail.com>

--- src/screens/UpdateInfoScreen.js
+++ src/screens/UpdateInfoScreen.js
@@ -1,147 +1,149 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import {StyleSheet, TouchableOpacity, Button, Text, TextInput, View, Image} from 'react-native'
+import React from 'react';
+import { connect } from 'react-redux';
+import { StyleSheet,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  View,
+  Image }
+  from 'react-native';
+
 const iconEmail = require('../images/ic_account_circle.png');
 const iconPhone = require('../images/ic_phone.png');
 
-class UpdateInfoScreen extends React.Component{
-  constructor(props){
+const styles = StyleSheet.create({
+  header: {
+    flex: 1.2,
+    backgroundColor: '#FF9500',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    shadowColor: 'black',
+    shadowOpacity: 1,
+    shadowRadius: 6,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+
+  textLogo: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 30,
+  },
+
+  principal: {
+    flex: 1,
+  },
+
+  buttonContainer: {
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderRadius: 7,
+    marginHorizontal: 15,
+    marginTop: 30,
+    marginBottom: 20,
+    backgroundColor: '#FF9500',
+    justifyContent: 'flex-end',
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFF',
+  },
+
+  content: {
+
+    marginBottom: 9,
+    flex: 6,
+    flexDirection: 'column',
+
+  },
+
+
+  inputs: {
+    backgroundColor: '#FAFAFA',
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderRadius: 7,
+    borderColor: 'gray',
+    marginHorizontal: 15,
+    marginBottom: 30,
+    justifyContent: 'flex-start',
+    paddingLeft: 2,
+    paddingRight: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  },
+
+
+  icon: {
+    width: 30,
+    height: 30,
+    margin: 5,
+  },
+});
+
+class UpdateInfoScreen extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       email: '',
-      phone:''
+      phone: '',
     };
   }
-  render(){
-    return(
+  render() {
+    return (
 
-        <View style={styles.principal}>
-          <View style={styles.topo}>
-            <Text style={styles.textLogo}>EDITAR INFORMAÇÕES</Text>
-          </View>
-
-          <View style={styles.conteudo}>
-            <View style={styles.inputs}>
-              <Image source={iconEmail} style={styles.icon}/>
-                <TextInput
-                  width = {280}
-                  maxLength = {50}
-                  placeholder="nome@exemplo.com"
-                  placeholderTextColor="black"
-                  underlineColorAndroid="transparent"
-                  onChangeText={(email) => this.setState({email})}>
-                </TextInput>
-            </View>
-            <View style={styles.inputs}>
-              <Image source={iconPhone} style={styles.icon}/>
-                <TextInput
-                  width = {280}
-                  maxLength = {11}
-                  placeholder="(00)00000-0000"
-                  placeholderTextColor="black"
-                  underlineColorAndroid="transparent"
-                  onChangeText={(phone) => this.setState({phone})}>
-                </TextInput>
-              </View>
-            </View>
-            <TouchableOpacity
-              style= {styles.buttonContainer}>
-              <Text style={styles.buttonText}>Concluir</Text>
-            </TouchableOpacity>
+      <View style={styles.principal}>
+        <View style={styles.header}>
+          <Text style={styles.textLogo}>EDITAR INFORMAÇÕES</Text>
         </View>
 
-    )
+        <View style={styles.content}>
+          <View style={styles.inputs}>
+            <TextInput
+              width = {280}
+              maxLength = {50}
+              placeholder="nome@exemplo.com"
+              placeholderTextColor="black"
+              underlineColorAndroid="transparent"
+              onChangeText={email => this.setState(email)}>
+              <Image source={iconEmail} style={styles.icon} />
+            </TextInput>
+            </View>
+          <View style={styles.inputs}>
+            <TextInput
+              width = {280}
+              maxLength = {11}
+              placeholder="(00)00000-0000"
+              placeholderTextColor="black"
+              underlineColorAndroid="transparent"
+              onChangeText={(phone) => this.setState({phone})}>
+              <Image source={iconPhone} style={styles.icon}/>
+            </TextInput>
+          </View>
+        </View>
+        <TouchableOpacity
+          style= {styles.buttonContainer}>
+          <Text style={styles.buttonText}>Concluir</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
 
-const mapStateToProps = (state) =>{
-  return{
+const mapStateToProps = () => {
+  console.log(),
+  return {
+  };
+};
 
-  }
-}
+const mapDispatchToProps = () => {
+  console.log(),
+  return {
+  };
+};
 
-const mapDispatchToProps = (dispatch) =>{
-  return{
-
-  }
-}
-
-const AppContainer = connect(mapStateToProps, mapDispatchToProps)(UpdateInfoScreen);
-
-
-const styles = StyleSheet.create({
-  topo: {
-      flex: 1.2,
-      backgroundColor: '#FF9500',
-      borderBottomColor: 'black',
-      borderBottomWidth: 1,
-      shadowColor: 'black',
-      shadowOpacity: 1,
-      shadowRadius: 6,
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-      marginBottom:50
-    },
-
-    textLogo: {
-        fontSize: 30,
-        color:'white',
-        fontWeight:'bold',
-        marginTop:30,
-    },
-
-    principal: {
-      flex: 1,
-    },
-
-    buttonContainer: {
-      paddingVertical:10,
-      borderWidth: 1,
-      borderRadius: 7,
-      marginHorizontal: 15,
-      marginTop: 30,
-      marginBottom: 20,
-      backgroundColor: '#FF9500',
-      justifyContent: 'flex-end'
-    },
-
-    buttonText:{
-      textAlign: 'center',
-      color: '#FFF'
-    },
-
-    conteudo: {
-
-        marginBottom: 9,
-        flex: 6,
-        flexDirection: 'column'
-
-    },
-
-
-    inputs: {
-      backgroundColor : '#FAFAFA',
-      paddingVertical:10,
-      borderWidth: 1,
-      borderRadius: 7,
-      borderColor: 'gray',
-      marginHorizontal: 15,
-      marginBottom: 30,
-      justifyContent: 'flex-start',
-      paddingLeft: 2,
-      paddingRight: 4,
-      flexDirection: 'row',
-      alignItems: 'center',
-
-    },
-
-
-    icon: {
-         width: 30,
-         height: 30,
-         margin: 5
-     }
-});
-
-
-export default AppContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UpdateInfoScreen);
