634d37b387d5ba441cfa1ac1286406d9bafda769
Gabriela Chaves de Moraes <gabrielademoraes10@gmail.com>
2017-10-29 20:02:20 -0200

    #39 - Adding Header and working on the screen.
    Signed-off-by: Miguel-Alves <miguelhenriquealvesdeoliveira@gmail.com>
    Signed-off-by: Gabriela Chaves de Moraes <gabrielademoraes10@gmail.com>

--- src/screens/SchoolInfoScreen.js
+++ src/screens/SchoolInfoScreen.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import axios from 'axios';
-import { View, Text } from 'react-native';
-
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import Header from '../components/Header';
 import { SCHOOL_ENDPOINT } from '../constants';
 import { logInfo, logWarn } from '../../logConfig/loggers';
 
 const FILE_NAME = 'SchoolInfoScreen.js';
+
+const styles = StyleSheet.create({
+  principal: {
+    flex: 1,
+  },
+
+  buttonContainer: {
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderRadius: 7,
+    marginHorizontal: 15,
+    marginTop: 30,
+    marginBottom: 20,
+    backgroundColor: '#FF9500',
+    justifyContent: 'flex-end',
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFF',
+  },
+
+  content: {
+
+    marginBottom: 9,
+    flex: 6,
+    flexDirection: 'column',
+  },
+
+  checkbox: {
+    flex: 1,
+    paddingTop: 10,
+    paddingLeft: 10,
+    flexWrap: 'wrap',
+  },
+  text: {
+    flexDirection: 'row',
+  },
+  label: {
+    paddingTop: 15,
+    flex: 1,
+  },
+
+});
 
 class SchoolInfoScreen extends React.Component {
 
@@ -51,11 +95,16 @@ class SchoolInfoScreen extends React.Component {
 
   render() {
     return (
-      <View>
+      <ScrollView style={styles.content}>
+        <Header
+          title={'Pesquisar Escola'}
+        />
+        <Text />
+        <Text> Infomações </Text>
         <Text style={{ color: 'black', fontSize: 20 }}>Nome:{this.props.school.schoolName}</Text>
         <Text style={{ color: 'black', fontSize: 20 }}>Email:{this.props.school.schoolEmail}</Text>
         <Text style={{ color: 'black', fontSize: 20 }}>Telefone:{this.props.school.schoolPhone}</Text>
-      </View>
+      </ScrollView>
     );
   }
 }
