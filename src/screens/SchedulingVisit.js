f84e88b9dd40471ce7adec3e6a860fd63a27e460
KahCosta <kamillacosta.unb@gmail.com>
2017-10-29 02:24:25 -0200

    #21 - Adding the fields: search school, invite counselor and invite health agent.
    Signed-off-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Signed-off-by: Igor Gabriel <igorgabriel515@gmail.com>
    Signed-off-by: KahCosta <kamillacosta.unb@gmail.com>

--- src/screens/SchedulingVisit.js
+++ src/screens/SchedulingVisit.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, TextInput, Image } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import Header from '../components/Header';
+
+const SearchIcon = require('../images/ic_search_48pt.png');
 
 const styles = StyleSheet.create({
 
@@ -10,10 +12,10 @@ const styles = StyleSheet.create({
   },
 
   buttonContainer: {
-    paddingVertical: 20,
+    paddingVertical: 12,
     borderWidth: 1,
     borderRadius: 7,
-    marginHorizontal: 15,
+    marginHorizontal: 20,
     marginTop: 30,
     marginBottom: 20,
     backgroundColor: '#FF9500',
@@ -24,7 +26,22 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#FFF',
   },
-
+  Input: {
+    marginTop: 5,
+    paddingLeft: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'gray',
+    backgroundColor: '#FAFAFA',
+    borderWidth: 1,
+    borderRadius: 7,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    margin: 5,
+  },
 });
 
 
@@ -39,34 +56,80 @@ export default class SchedulingVisit extends React.Component {
   render() {
     return (
       <View style={styles.principal}>
-        <Header title={'Agendar Visita'} />
-
+        <Header
+          title={'AGENDAR'}
+          subTitle={'VISITA'}
+        />
+        <View style={styles.Input}>
+          <Image source={SearchIcon} style={styles.icon} />
+          <TextInput
+            width={280}
+            returnKeyType="go"
+            maxLength={50}
+            keyboardType={'default'}
+            onChangeText={text => this.validateName(text)}
+            value={this.state.name}
+            underlineColorAndroid="transparent"
+            placeholder="Nome da escola"
+          />
+        </View>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>HORA SELECIONADA: {this.state.time}</Text>
-          <Text>DATA SELECIONADA: {this.state.date}</Text>
-
           <DatePicker
-            placeholder="escolha uma data"
-            style={{ width: '90%' }}
+            placeholder="Data"
+            style={{ width: '70%' }}
             mode="date"
             format="DD-MM-YYYY"
             confirmBtnText="Confirmar"
             cancelBtnText="Cancelar"
             onDateChange={date => this.setState({ date })}
           />
+          <Text> Data escolhida: {this.state.date}</Text>
+
           <DatePicker
-            placeholder="escolha uma hora"
-            style={{ width: '90%' }}
+            placeholder="Hora"
+            style={{ width: '70%' }}
             mode="time"
             confirmBtnText="Confirmar"
             cancelBtnText="Cancelar"
             onDateChange={time => this.setState({ time })}
           />
+          <Text> Hora escolhida: {this.state.time}</Text>
+
+        </View>
+        <Text>     Convidar Conselheiro</Text>
+        <View style={styles.Input}>
+          <Image source={SearchIcon} style={styles.icon} />
+          <TextInput
+            width={280}
+            returnKeyType="go"
+            maxLength={50}
+            keyboardType={'default'}
+            onChangeText={text => this.validateName(text)}
+            value={this.state.name}
+            underlineColorAndroid="transparent"
+            placeholder="Nome do Conselheiro"
+          />
+        </View>
+        <Text>     Convidar Agente Sanitário</Text>
+        <View style={styles.Input}>
+          <Image source={SearchIcon} style={styles.icon} />
+          <TextInput
+            width={280}
+            returnKeyType="go"
+            maxLength={50}
+            keyboardType={'default'}
+            onChangeText={text => this.validateName(text)}
+            value={this.state.name}
+            underlineColorAndroid="transparent"
+            placeholder="Digite o e-mail do Agente"
+          />
+        </View>
+        <View>
           <TouchableOpacity
             style={styles.buttonContainer}
-            onPress={() => Alert.alert('Clique realizado')}
+            onPress={() => Alert.alert('Agendamento realizado com sucesso!')}
           >
-            <Text style={styles.buttonText}>Concluir Agendamento de Visita</Text>
+            <Text style={styles.buttonText}>Agendar</Text>
           </TouchableOpacity>
         </View>
       </View>
