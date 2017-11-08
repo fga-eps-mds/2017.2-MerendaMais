431f1f7ce422ade98b077f2b456e24e340004778
Miguel Alves <miguelhenriquealvesdeoliveira@gmail.com>
2017-11-08 19:17:40 -0200

    #71 - Adding MainReports Screen test.
    Signed-off-by: mlfaa <mlfaa@hotmail.com>
    Signed-off-by: Miguel Alves <miguelhenriquealvesdeoliveira@gmail.com>

---
+++ __tests__/screens/MainReportsScreen.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import configureStore from 'redux-mock-store';
+// imported as a connected component!
+import MainReportsScreen from '../../src/screens/MainReportsScreen';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockStore = configureStore();
+
+const initialState = {
+  report: {
+  },
+};
+
+const store = mockStore(initialState);
+
+describe('Testing DocCheckout Screen', () => {
+  it('renders as expected', () => {
+    const wrapper = shallow(
+      <MainReportsScreen />,
+      { context: { store } },
+    ).dive();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
