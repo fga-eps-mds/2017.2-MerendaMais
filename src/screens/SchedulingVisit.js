bd5a6adcd558ce6c1c574b012966d720cf4fa61f
LucasPenido <lpenidoa@me.com>
2017-11-14 18:41:20 -0200

    #23 - Changing SchedulingVisit screen Style.
    Signed-off-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Signed-off-by: LucasPenido <lpenidoa@me.com>

--- src/screens/SchedulingVisit.js
+++ src/screens/SchedulingVisit.js
@@ -14,37 +14,14 @@ const styles = StyleSheet.create({
 
   principal: {
     flex: 1,
-  },
-
-  schedullingButton: {
-    paddingVertical: 20,
-    borderWidth: 1,
-    borderRadius: 7,
-    marginHorizontal: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#FF9500',
-    justifyContent: 'flex-end',
-  },
-
-  disabledSchedullingButton: {
-    paddingVertical: 20,
-    borderWidth: 1,
-    borderRadius: 7,
-    marginHorizontal: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#DEDEDE',
-    justifyContent: 'flex-end',
+    backgroundColor: 'white',
   },
 
   button: {
     paddingVertical: 15,
     borderWidth: 1,
     borderRadius: 7,
-    marginHorizontal: 20,
-    marginTop: 10,
-    marginBottom: 10,
+    marginVertical: 10,
     backgroundColor: '#FF9500',
     justifyContent: 'flex-end',
   },
@@ -55,40 +32,55 @@ const styles = StyleSheet.create({
   },
 
   Container: {
-    flex: 1,
-    marginTop: 20,
+    marginTop: 15,
+    marginHorizontal: 20,
   },
 
   Picker: {
-    marginHorizontal: 15,
-    paddingLeft: 10,
-    width: '95%',
-  },
-
-  icon: {
-    width: 20,
-    height: 20,
-    margin: 5,
+    width: null,
   },
 
   textBox: {
+    margin: 1.5,
     paddingLeft: 2,
     justifyContent: 'flex-start',
   },
+
   text: {
     fontSize: 15,
-    paddingVertical: 5,
+    paddingVertical: 3,
   },
 
   listRegisters: {
-    flex: 1,
-    marginHorizontal: 15,
+    marginHorizontal: 10,
     marginVertical: 5,
     borderColor: 'black',
     borderWidth: 1.5,
-    borderRadius: 3,
+    borderRadius: 7,
     backgroundColor: '#FAFAFA',
     justifyContent: 'space-between',
+  },
+
+  invitedList: {
+    borderColor: 'black',
+    borderWidth: 1.5,
+    marginHorizontal: 20,
+    borderRadius: 5,
+  },
+
+  dialogButtonStyle: {
+    marginVertical: -16,
+  },
+
+  footerPopUp: {
+    backgroundColor: '#F9F9FB',
+    borderColor: '#DAD9DC',
+    borderTopWidth: 0.5,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
 });
@@ -167,12 +159,24 @@ export default class SchedulingVisit extends React.Component {
   }
 
   showInvitedList() {
-    if (this.statelistOfInviteesWithCounselorInformations !== {}) {
+    // Check if the Object is empty
+    if (Object.keys(this.state.listOfInviteesWithCounselorInformations)
+      .length !== 0) {
       return (
-        Object.entries(this.state.listOfInviteesWithCounselorInformations)
-          .map(counselor => (
-            <InvitedCounselorsData {...counselor[1]} />
-          ))
+        <View>
+          <Text style={{ fontSize: 20, textAlign: 'center', marginVertical: 10 }}>Lista de conselheiros convidados</Text>
+          <View style={styles.invitedList}>
+            {
+              Object.entries(this.state.listOfInviteesWithCounselorInformations)
+                .map(counselor => (
+                  <InvitedCounselorsData
+                    key={counselor[0]}
+                    {...counselor[1]}
+                  />
+                ))
+            }
+          </View>
+        </View>
       );
     }
     return null;
@@ -214,27 +218,27 @@ export default class SchedulingVisit extends React.Component {
           backButton
         />
         <PopupDialog
-          style={{ flex: 1 }}
           ref={(popupDialog) => { this.popupDialog = popupDialog; }}
-          dialogTitle={<DialogTitle title="Conselheiros do seu CAE" />}
+          dialogTitle={<DialogTitle title="Escolha quem deseja convidar" />}
+          overlayPointerEvents="none"
+          height="80%"
+          width="85%"
           actions={[
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <View style={styles.footerPopUp}>
               <DialogButton
-                buttonStyle={{ marginVertical: -18 }}
+                buttonStyle={styles.dialogButtonStyle}
                 text="ACEITAR"
                 onPress={() => this.popupDialog.dismiss()}
                 key="dialogButton1"
               />
               <DialogButton
-                buttonStyle={{ marginVertical: -18 }}
+                buttonStyle={styles.dialogButtonStyle}
                 text="CANCELAR"
                 onPress={() => this.cancelInviteList()}
                 key="dialogButton2"
               />
             </View>,
           ]}
-          height="80%"
-          width="85%"
         >
           <ScrollView key="showInviteCounselorList">
             {this.renderCounselorList()}
@@ -242,8 +246,8 @@ export default class SchedulingVisit extends React.Component {
         </PopupDialog>
 
         <ScrollView>
-          <View style={styles.Container}>
-            <View>
+          <View>
+            <View style={styles.Container}>
               <TouchableOpacity
                 key="searchSchoolButton"
                 style={styles.button}
@@ -252,49 +256,47 @@ export default class SchedulingVisit extends React.Component {
                 <Text style={styles.buttonText}>Pesquisar escola</Text>
               </TouchableOpacity>
             </View>
-          </View>
 
-          {this.props.school.schoolSelected && (
-            <SchoolData {...this.props.school} />
-          )}
+            {this.props.school.schoolSelected && (
+              <SchoolData {...this.props.school} />
+            )}
 
-          <View style={styles.Container}>
-            <DatePicker
-              style={styles.Picker}
-              placeholder="Data"
-              date={this.state.visit.date}
-              mode="date"
-              format="DD-MM-YYYY"
-              confirmBtnText="Confirmar"
-              cancelBtnText="Cancelar"
-              customStyles={{
-                dateInput: {
-                  borderRadius: 7,
-                },
-              }}
-              onDateChange={date => this.setState({ visit: { ...this.state.visit, date } })}
-            />
-          </View>
+            <View style={[styles.Container, { marginVertical: 10 }]}>
+              <DatePicker
+                style={styles.Picker}
+                placeholder="Data"
+                date={this.state.visit.date}
+                mode="date"
+                format="DD-MM-YYYY"
+                confirmBtnText="Confirmar"
+                cancelBtnText="Cancelar"
+                customStyles={{
+                  dateInput: {
+                    borderRadius: 7,
+                  },
+                }}
+                onDateChange={date => this.setState({ visit: { ...this.state.visit, date } })}
+              />
+            </View>
 
-          <View style={styles.Container}>
-            <DatePicker
-              style={styles.Picker}
-              placeholder="Horário"
-              date={this.state.visit.time}
-              mode="time"
-              confirmBtnText="Confirmar"
-              cancelBtnText="Cancelar"
-              customStyles={{
-                dateInput: {
-                  borderRadius: 7,
-                },
-              }}
-              onDateChange={time => this.setState({ visit: { ...this.state.visit, time } })}
-            />
-          </View>
+            <View style={styles.Container}>
+              <DatePicker
+                style={styles.Picker}
+                placeholder="Horário"
+                date={this.state.visit.time}
+                mode="time"
+                confirmBtnText="Confirmar"
+                cancelBtnText="Cancelar"
+                customStyles={{
+                  dateInput: {
+                    borderRadius: 7,
+                  },
+                }}
+                onDateChange={time => this.setState({ visit: { ...this.state.visit, time } })}
+              />
+            </View>
 
-          <View style={styles.Container}>
-            <View>
+            <View style={styles.Container}>
               <TouchableOpacity
                 key="searchCounselorButton"
                 style={styles.button}
@@ -303,12 +305,10 @@ export default class SchedulingVisit extends React.Component {
                 <Text style={styles.buttonText}>Pesquisar Conselheiro</Text>
               </TouchableOpacity>
             </View>
-          </View>
 
-          {this.showInvitedList()}
+            {this.showInvitedList()}
 
-          <View style={styles.Container}>
-            <View>
+            <View style={styles.Container}>
               <TouchableOpacity
                 key="searchAgentButton"
                 style={styles.button}
@@ -317,24 +317,25 @@ export default class SchedulingVisit extends React.Component {
                 <Text style={styles.buttonText}>Pesquisar Agente Sanitário</Text>
               </TouchableOpacity>
             </View>
-          </View>
-          <View style={styles.Container}>
-            {this.props.school.schoolSelected && (
-              <Button
-                enabled
-                key="scheduleButton"
-                text="Agendar"
-                onPress={() => { this.props.asyncSchedulingVisit(this.state); }}
-              />
-            )}
 
-            <Button
-              enabled={false}
-              text="Agendar"
-              key="scheduleButton"
-              onPress={() => ({})}
-              disabled
-            />
+            <View>
+              {this.props.school.schoolSelected && (
+                <Button
+                  enabled
+                  key="scheduleButton"
+                  text="Agendar"
+                  onPress={() => { this.props.asyncSchedulingVisit(this.state); }}
+                />
+              )}
+
+              <Button
+                enabled={false}
+                text="Agendar"
+                key="scheduleButton"
+                onPress={() => ({})}
+                disabled
+              />
+            </View>
           </View>
         </ScrollView>
       </View>
