cdf5051b663c031259d88898a640cd8f786d1ac7
LucasPenido <lpenidoa@me.com>
2017-11-14 18:42:28 -0200

    #23 - Changing the component InvitedCounselorData style.
    Signed-off-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Signed-off-by: LucasPenido <lpenidoa@me.com>

--- src/components/InvitedCounselorsData.js
+++ src/components/InvitedCounselorsData.js
@@ -3,51 +3,33 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   listRegisters: {
-    flex: 1,
-    marginHorizontal: 15,
-    marginVertical: 10,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 3,
-    backgroundColor: '#FAFAFA',
-    justifyContent: 'space-between',
-  },
-  title: {
-    marginHorizontal: 15,
-    marginVertical: 10,
-    justifyContent: 'center',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-
-  textBox: {
+    marginVertical: 5,
+    backgroundColor: 'white',
     paddingLeft: 5,
     justifyContent: 'flex-start',
   },
+
   text: {
     fontSize: 15,
-    paddingVertical: 5,
+    paddingVertical: 2,
   },
 });
 
 const InvitedCounselorsData = counselor => (
 
   <View style={styles.listRegisters}>
-    <View style={styles.textBox}>
-      <Text style={styles.title}>CONSELHEIRO SELECIONADO</Text>
-      <Text style={styles.text}>
-        <Text style={{ fontWeight: 'bold' }}>Nome: </Text>
-        {counselor.name}
-      </Text>
-      <Text style={styles.text}>
-        <Text style={{ fontWeight: 'bold' }}>CPF: </Text>
-        {counselor.cpf}
-      </Text>
-      <Text style={styles.text}>
-        <Text style={{ fontWeight: 'bold' }}>Telefone: </Text>
-        {counselor.phone}
-      </Text>
-    </View>
+    <Text style={styles.text}>
+      <Text style={{ fontWeight: 'bold' }}>Nome: </Text>
+      {counselor.name}
+    </Text>
+    <Text style={styles.text}>
+      <Text style={{ fontWeight: 'bold' }}>CPF: </Text>
+      {counselor.cpf}
+    </Text>
+    <Text style={styles.text}>
+      <Text style={{ fontWeight: 'bold' }}>Telefone: </Text>
+      {counselor.phone}
+    </Text>
   </View>
 );
 
