6075580e9e6d1a6cfe0100810707da16155466f0
LucasPenido <lpenidoa@me.com>
2017-11-15 11:54:51 -0200

    #23 - Creating Reducer that sends the list of invitees with informations to store.
    Signed-off-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Signed-off-by: LucasPenido <lpenidoa@me.com>

--- src/Reducers/listReducer.js
+++ src/Reducers/listReducer.js
@@ -1,5 +1,7 @@
 import initialState from './initialState';
-import { SET_LIST_COUNSELOR_GROUP, RESET_LIST } from '../actions/types';
+import { SET_LIST_COUNSELOR_GROUP,
+  RESET_LIST,
+  SET_LIST_OF_INVITEES_WITH_COUNSELOR_INFORMATIONS } from '../actions/types';
 
 const listReducer = (state = initialState.list, action) => {
   if (action === undefined) {
@@ -9,11 +11,19 @@ const listReducer = (state = initialState.list, action) => {
   switch (action.type) {
     case SET_LIST_COUNSELOR_GROUP:
       return {
+        ...state,
         listOfCounselorsInAGroup: [...state.listOfCounselorsInAGroup, action.payload],
       };
     case RESET_LIST:
       return {
+        ...state,
         listOfCounselorsInAGroup: [],
+      };
+    case SET_LIST_OF_INVITEES_WITH_COUNSELOR_INFORMATIONS:
+      console.log('entrou');
+      return {
+        ...state,
+        listOfInviteesWithCounselorInformations: action.payload,
       };
     default:
       return state;
