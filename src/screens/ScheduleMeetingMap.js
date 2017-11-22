93e342aba83694584c139a28a2697b2bc93fdda1
Miguel Alves <miguelhenriquealvesdeoliveira@gmail.com>
2017-11-21 23:03:07 -0200

    #22 - Adding search Location Field and working on styles.
    Signed-off-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Signed-off-by: Gabriela Moraes <gabrielademoraes10@gmail.com>
    Signed-off-by: Miguel Alves <miguelhenriquealvesdeoliveira@gmail.com>

--- src/screens/ScheduleMeetingMap.js
+++ src/screens/ScheduleMeetingMap.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import { MapView } from 'expo';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity, Dimensions, KeyboardAvoidingView, Alert, ScrollView } from 'react-native';
-import Header from '../components/Header';
+import { StyleSheet, Text, TextInput, View, Dimensions, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Header from '../components/Header';
 
+const { height } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   principal: {
     flex: 1,
     backgroundColor: 'white',
   },
+  button: {
+    paddingVertical: 15,
+    borderWidth: 2,
+    borderRadius: 7,
+    marginBottom: 5,
+    backgroundColor: '#FF9500',
+    justifyContent: 'flex-end',
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFF',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 12,
+    marginTop: 5,
+    paddingTop: 10,
+    paddingLeft: 10,
+    height: height * 0.06,
+    fontSize: width * 0.05,
+    textAlignVertical: 'top',
+  },
+  textBox: {
+    margin: 1.5,
+    paddingLeft: 2,
+    justifyContent: 'flex-start',
+  },
+
 });
 
 
@@ -52,6 +82,13 @@ export default class ScheduleMeetingMap extends React.Component {
           subTitle={'ESCOLHA O LOCAL'}
           backButton
         />
+        <View style={styles.textBox}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Local da Reunião"
+            // onChangeText={text => this.setState({ passwordCompared: text })}
+          />
+        </View>
         <MapView
           provider={MapView.PROVIDER_GOOGLE}
           style={{ flex: 1 }}
@@ -62,6 +99,13 @@ export default class ScheduleMeetingMap extends React.Component {
             pinColor="orange"
           />
         </MapView>
+        <TouchableOpacity
+          key="setMeetingLocationButton"
+          style={styles.button}
+          // onPress={() => Actions.ScheduleMeetingMap()}
+        >
+          <Text style={styles.buttonText}>Definir Local da Reunião</Text>
+        </TouchableOpacity>
       </View>
     );
   }
