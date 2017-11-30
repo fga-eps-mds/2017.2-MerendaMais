fbbaa66a52c010db958cf79ae7b22fa16f86baaf
LucasLermen <lucas.arthur.lermen@gmail.com>
2017-11-30 14:47:28 -0200

    #22 - Fixing meeting post

--- src/actions/schedulingMeetingActions.js
+++ src/actions/schedulingMeetingActions.js
@@ -60,7 +60,7 @@ export const schedulingMeeting = (meetingData, dispatch) => {
     },
   };
 
-  axios.post(POSTS_LINK_NUVEM_CIVICA, bodyToSchedulingMeeting, headerToSchedulingMeeting)
+  axios.post(`${POSTS_LINK_NUVEM_CIVICA}conteudos`, bodyToSchedulingMeeting, headerToSchedulingMeeting)
     .then((response) => {
       logInfo(FILE_NAME, 'schedulingMeeting',
         `Scheduling made in Nuvem cívica: ${JSON.stringify(response.data, null, 2)}`);
@@ -68,7 +68,7 @@ export const schedulingMeeting = (meetingData, dispatch) => {
       dispatch(isNotLoading());
       Actions.mainScreen();
     })
-    .catch((error) => {
+    .catch((response, error) => {
       logWarn(FILE_NAME, 'schedulingMeeting',
         `Request result in an ${error}`);
 
