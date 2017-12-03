d27bc563818aca80bfdd833161c6a8999a411d73
Hugo Alves <hugo.asb96@gmail.com>
2017-12-03 01:03:21 -0200

    #16 - Adding competence agencies contact informations.
    Signed-off-by: LucasLermen <lucas.arthur.lermen@gmail.com>
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>

---
+++ src/constants/publicAgencyInformations.js
@@ -0,0 +1,28 @@
+// Constants used on the complaintScreen.
+
+export const COMPLAINT_TITLE = 'Quer fazer uma denúncia? Entre em contato com os órgãos competentes:';
+
+export const ADDRESS = 'Endereço: ';
+export const WEB_SITE = 'Endereço na internet: ';
+export const EMAIL = 'E-mail: ';
+export const PHONE = 'Telefone: ';
+
+export const FNDE = 'Fundo Nacional de Desenvolvimento da Educação (FNDE)';
+export const FNDE_ADDRESS = 'SBS - Quadra 2, Bloco F, Edifício FNDE Brasília-DF CEP: 70070.929';
+export const FNDE_PHONE = '0800-616161';
+export const FNDE_WEB_SITE = 'www.fnde.gov.br';
+export const FNDE_EMAIL = 'ouvidoria@fnde.gov.br | audit@fnde.gov.br';
+
+export const CGU = 'Ministério da Transparência, Fiscalização e Controladoria-Geral da União (CGU)';
+export const CGU_ADDRESS = 'SAS - Quadra 1, Bloco A, 8º andar Edifício Darcy Ribeiro Brasília-DF CEP: 70.070-905';
+export const CGU_WEB_SITE = 'http://www.cgu.gov.br/assuntos/ouvidoria/denuncias-e-manifestacoes';
+
+export const MPF = 'Ministério Público Federal';
+export const MPF_ADDRESS = 'SAFS, Quadra 04, conjunto “C” Brasília-DF CEP: 70050-900';
+export const MPF_PHONE = '(61) 3031-5100';
+export const MPF_EMAIL = 'http://www.mpf.mp.br/para-o-cidadao/sac';
+
+export const TCU = 'Tribunal de Contas da União';
+export const TCU_ADDRESS = 'SAFS - Quadra 04, Lote 01 Brasília-DF CEP: 70042-900';
+export const TCU_PHONE = '0800-6441500';
+export const TCU_EMAIL = 'www.tcu.gov.br/ouvidoria';
--- src/screens/complaintScreen.js
+++ src/screens/complaintScreen.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import Header from '../components/Header';
+import * as constant from '../constants/publicAgencyInformations';
 
 const styles = StyleSheet.create({
-
   complaintScreen: {
     flex: 1,
+  },
+
+  textBox: {
+    marginTop: 20,
+    paddingLeft: 2,
+    justifyContent: 'flex-start',
+  },
+
+  field: {
+    backgroundColor: '#FAFAFA',
+    paddingVertical: 4,
+    borderWidth: 1,
+    borderRadius: 7,
+    borderColor: 'gray',
+    marginHorizontal: 15,
+    marginBottom: 15,
+    marginTop: 15,
+    justifyContent: 'flex-start',
+    paddingLeft: 2,
+    paddingRight: 4,
+  },
+
+  information: {
+    fontSize: 15,
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 
 });
@@ -15,9 +45,78 @@ const complaintScreen = () => ({
     return (
       <View style={styles.complaintScreen}>
         <Header
-          title={'Como denunciar?'}
+          title={'Denunciar'}
           backButton
         />
+        <ScrollView>
+          <View style={styles.textBox}>
+            <Text style={[styles.title, { fontSize: 20 }]}>{constant.COMPLAINT_TITLE}</Text>
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.title}>{constant.FNDE}</Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.ADDRESS}</Text>
+              {constant.FNDE_ADDRESS}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.PHONE}</Text>
+              {constant.FNDE_PHONE}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.WEB_SITE}</Text>
+              {constant.FNDE_WEB_SITE}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.EMAIL}</Text>
+              {constant.FNDE_EMAIL}
+            </Text>
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.title}>{constant.CGU}</Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.ADDRESS}</Text>
+              {constant.CGU_ADDRESS}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.WEB_SITE}</Text>
+              {constant.CGU_WEB_SITE}
+            </Text>
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.title}>{constant.MPF}</Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.ADDRESS}</Text>
+              {constant.MPF_ADDRESS}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.PHONE}</Text>
+              {constant.MPF_PHONE}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.EMAIL}</Text>
+              {constant.MPF_EMAIL}
+            </Text>
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.title}>{constant.TCU}</Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.ADDRESS}</Text>
+              {constant.TCU_ADDRESS}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.PHONE}</Text>
+              {constant.TCU_PHONE}
+            </Text>
+            <Text style={styles.information}>
+              <Text style={{ fontWeight: 'bold' }}>{constant.EMAIL}</Text>
+              {constant.TCU_EMAIL}
+            </Text>
+          </View>
+        </ScrollView>
       </View>
     );
   },
