e9be18003b7a5cc9c93b4f8f59cff4401599cad2
Gustavo Sabino <gustavo.sabino@hotmail.com>
2018-04-10 00:21:39 -0300

    Verificacao inicial do texto em GenericField

--- src/components/GenericField.js
+++ src/components/GenericField.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text, View, TextInput } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import styles from '../Styles';
 
 
-const GenericField = props => (
+export default class GenericField extends Component {
 
-  <View>
-    <Text> {props.header.toUpperCase()} </Text>
-    <TextInput
-      style={styles.InputFieldStyle}
-      placeholder={props.message}
-    >
-      <FontAwesome name={props.icon} style={styles.icon} size={26} color="black" />
-    </TextInput>
-  </View>
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      validateMessage: '',
+    };
+  }
 
-);
+  verificaTexto(texto) {
+    // arrumar essa validação para receber um Regex
 
+    if (texto === 'texto') {
+      console.warn('Texto correto');
+      this.setState({ validateMessage: true });
+    } else {
+      console.warn('Texto Incorreto');
+      this.setState({ validateMessage: false });
+    }
+  }
+
+  render() {
+    // Provavelmente mudar isso para um Style diferente. User uma linha vermelha, nao sei.
+    // Não usar uma msg.
+    let p;
+    if (!this.state.validateMessage) {
+      p = 'Erro';
+    } else {
+      p = 'Correto';
+    }
+    return (
+      <View>
+        <Text> {this.props.header.toUpperCase()} </Text>
+        <FontAwesome name={this.props.icon} style={styles.icon} size={26} color="black" />
+        <Text>{p}</Text>
+        <TextInput
+          style={styles.InputFieldStyle}
+          placeholder={this.props.message}
+          onChangeText={text => this.verificaTexto(text)}
+        />
+
+      </View>
+    );
+  }
+}
 GenericField.propTypes = {
   header: PropTypes.string.isRequired,
   message: PropTypes.string.isRequired,
@@ -27,4 +58,4 @@ GenericField.propTypes = {
 };
 
 
-export default GenericField;
+// export default GenericField;
