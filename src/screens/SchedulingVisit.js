90fa6ccca0df2eeb2050b4bfd070ebe8d79ab227
ThalissonMelo <tallisonmelo@hotmail.com>
2018-06-20 22:21:04 -0300

    Fixing Styles of SchedulingVisit

--- src/screens/SchedulingVisit.js
+++ src/screens/SchedulingVisit.js
@@ -1,7 +1,7 @@
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Dimensions, BackHandler, ActivityIndicator, Alert } from 'react-native';
+import { Text, View, TouchableOpacity, ScrollView, BackHandler, ActivityIndicator, Alert } from 'react-native';
 import PopupDialog, {
   DialogTitle,
   DialogButton,
@@ -17,168 +17,7 @@ import { backHandlerPopToMain } from '../NavigationFunctions';
 import Header from '../components/Header';
 import ShowToast from '../components/Toast';
 import { NO_OTHER_COUNSELORS } from '../constants/generalConstants';
-
-const { width } = Dimensions.get('window');
-
-const styles = StyleSheet.create({
-
-  principal: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  textLogo: {
-    // Font size 30 looks nice on 360 width phone.
-    // (x * widthYourPhone = fontSize) where x is the proportion used in fontSize above.
-    fontSize: width * 0.08,
-    color: 'white',
-    fontWeight: 'bold',
-    marginTop: 10,
-    marginLeft: 60,
-  },
-  wrapper: {
-    height: 100,
-    flexDirection: 'row',
-    paddingTop: 10,
-    paddingBottom: 10,
-    backgroundColor: '#FF9500',
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    alignItems: 'center',
-  },
-  schedullingButton: {
-    paddingVertical: 20,
-    borderWidth: 1,
-    borderRadius: 7,
-    marginHorizontal: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#FF9500',
-    justifyContent: 'flex-end',
-  },
-
-  disabledSchedullingButton: {
-    paddingVertical: 20,
-    borderWidth: 1,
-    borderRadius: 7,
-    marginHorizontal: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#DEDEDE',
-    justifyContent: 'flex-end',
-  },
-
-  button: {
-    paddingVertical: 15,
-    borderWidth: 1,
-    borderRadius: 7,
-    marginVertical: 10,
-    backgroundColor: '#FF9500',
-    justifyContent: 'flex-end',
-  },
-
-  buttonText: {
-    textAlign: 'center',
-    color: '#FFF',
-  },
-
-  Container: {
-    marginTop: 15,
-    marginHorizontal: 20,
-  },
-  icon_header: {
-    marginLeft: 20,
-  },
-  Picker: {
-    marginHorizontal: 15,
-    width: '95%',
-  },
-
-  popUp: {
-    flex: 1,
-    alignItems: 'center',
-    margin: 20,
-  },
-
-  popUpText: {
-    fontSize: 15,
-    textAlign: 'justify',
-    lineHeight: 20,
-  },
-
-  textBox: {
-    margin: 1.5,
-    paddingLeft: 2,
-    justifyContent: 'flex-start',
-  },
-
-  text: {
-    fontSize: 15,
-    paddingVertical: 3,
-  },
-
-  listRegisters: {
-    marginHorizontal: 10,
-    marginVertical: 5,
-    borderColor: 'black',
-    borderWidth: 1.5,
-    borderRadius: 7,
-    backgroundColor: '#FAFAFA',
-    justifyContent: 'space-between',
-  },
-
-  invitedList: {
-    borderColor: 'black',
-    height: 250,
-    borderWidth: 1.5,
-    padding: 1,
-    marginHorizontal: 20,
-    borderRadius: 5,
-  },
-
-  dialogButtonStyle: {
-    marginVertical: -16,
-  },
-
-  footerPopUp: {
-    backgroundColor: '#F9F9FB',
-    borderColor: '#DAD9DC',
-    borderTopWidth: 0.5,
-    borderBottomLeftRadius: 8,
-    borderBottomRightRadius: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  TopListText: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-
-  invitedListStyle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-
-  InputFieldStyle: {
-    marginHorizontal: 65,
-    marginLeft: 30,
-    marginRight: 30,
-    paddingVertical: 8,
-    marginTop: 1,
-    backgroundColor: '#FAFAFA',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 7,
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-  },
-
-});
+import styles from '../Styles/SchedulingVisitStyles'
 
 export default class SchedulingVisit extends React.Component {
   constructor(props) {
@@ -249,7 +88,7 @@ export default class SchedulingVisit extends React.Component {
       <View>
         <Text style={styles.TopListText}>Agente Convidado</Text>
         <View style={styles.InputFieldStyle}>
-          <Text style={{ color: '#bababa' }}>Nenhum agente convidado</Text>
+          <Text style={ styles.NoInvitedAgent }>Nenhum agente convidado</Text>
         </View>
       </View>
     );
@@ -357,7 +196,7 @@ export default class SchedulingVisit extends React.Component {
   renderCounselorList() {
     if (this.props.application === true) {
       return (
-        <ActivityIndicator style={{ marginTop: 50, justifyContent: 'center' }} size="large" color="#FF9500" />
+        <ActivityIndicator style={ styles.ConsuelorList } size="large" color="#FF9500" />
       );
     }
     return (
@@ -368,15 +207,15 @@ export default class SchedulingVisit extends React.Component {
           >
             <View style={styles.textBox}>
               <Text style={styles.text}>
-                <Text style={{ fontWeight: 'bold' }}>Nome: </Text>
+                <Text style={ styles.ConsuelorInformation }>Nome: </Text>
                 {counselor.name}
               </Text>
               <Text style={styles.text}>
-                <Text style={{ fontWeight: 'bold' }}>CPF: </Text>
+                <Text style={ styles.ConsuelorInformation }>CPF: </Text>
                 {counselor.profile.cpf}
               </Text>
               <Text style={styles.text}>
-                <Text style={{ fontWeight: 'bold' }}>Telefone: </Text>
+                <Text style={ styles.ConsuelorInformation }>Telefone: </Text>
                 {counselor.profile.phone}
               </Text>
             </View>
@@ -430,7 +269,6 @@ export default class SchedulingVisit extends React.Component {
               value={this.state.email}
               size={28}
             />
-
           </View>
         </PopupDialog>
 
